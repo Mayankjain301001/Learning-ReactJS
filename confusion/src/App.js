335a0b5119358622f7924b9ec795209ac67b0724
Mayank Jain <mayankfriend30.mj@gmail.com>
2021-02-27 23:06:16 +0530

    Configuring React

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-          <p className="App-intro">
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Restorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
       </div>
     );
    }
