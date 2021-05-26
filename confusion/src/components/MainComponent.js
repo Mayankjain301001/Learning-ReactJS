dce3468f0beb1d4cb91d255ce5fc723fb36caf39
Mayank Jain <mayankfriend30.mj@gmail.com>
2021-05-26 15:40:35 +0530

    About Component updated

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent'; 
@@ -52,6 +53,7 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path="/home" component={ HomePage } />
+          <Route path="/aboutus" component={ ()=> <About leaders={this.state.leaders} />} />
           <Route exact path="/menu" component={ () => <Menu dishes={ this.state.dishes } />  } />
           <Route path="/menu/:dishId" component={ DishWithId } />
           <Route exact path="/contactus" component={ Contact } />
